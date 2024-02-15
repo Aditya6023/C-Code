3130a4f2d9b33e4dee82589ab45f83d682fb93ef
Aditya <aditya60bh@gmail.com>
2024-02-15 23:26:59 +0530

    Arthmatic Operations

---
+++ 2.c
@@ -0,0 +1,26 @@
+// Arthmatic Operations 
+
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+
+    printf("Sum: %d\n", num1 + num2);
+    printf("Difference: %d\n", num1 - num2);
+    printf("Product: %d\n", num1 * num2);
+
+    if (num2 != 0) {
+        printf("Quotient: %d\n", num1 / num2);
+        printf("Remainder: %d\n", num1 % num2);
+    } else {
+        printf("Cannot divide by zero.\n");
+    }
+
+    return 0;
+}
