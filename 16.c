f858876423ef1e1ffcf0f48840f9df498b134fcf
Aditya <aditya60bh@gmail.com>
2024-02-17 00:48:21 +0530

    Addition

---
+++ 16.c
@@ -0,0 +1,22 @@
+// Addition
+
+#include <stdio.h>
+
+int main() {
+    int num1, num2, sum;
+
+    // Ask the user to input two numbers
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+    
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+
+    // Perform addition
+    sum = num1 + num2;
+
+    // Print the result
+    printf("The sum of %d and %d is %d.\n", num1, num2, sum);
+
+    return 0;
+}
